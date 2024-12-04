69937068065fd6877f7b3f5fae1c13811fc50769
DIEGOGIO123 <159564293+DIEGOGIO123@users.noreply.github.com>
2024-12-04 01:51:53 +0000

    Swipe to Adjust Counter (Touch Pan)

--- quasar-counter/src/pages/IndexPage.vue
+++ quasar-counter/src/pages/IndexPage.vue
@@ -1,23 +1,24 @@
 <template>
-  <q-page class="flex flex-center ">
+  <q-page 
+  v-touch-pan.vertical.prevent.mouse="handlePan"
+  class="flex flex-center ">
     <div class="row">
       <q-input
-      v-model="data.name"  
-      input-class="text-center text-h5"
-      color="teal"
-      placeholder="Counter" 
-      filled
+        v-model="data.name"  
+        input-class="text-center text-h5"
+        color="teal"
+        placeholder="Counter" 
+        filled
       />
     </div>
     <div class="row full-width items-center">
       <div class="col text-center">
         <q-btn 
-        @click="decreaseCounter"
-        v-touch-repeat:300:300:300:300:50.
-         mouse="decreaseCounter"
-        icon="remove" 
-        size="xl"
-        round
+          @click="decreaseCounter"
+          v-touch-repeat:300:300:300:300:50.mouse="decreaseCounter"
+          icon="remove" 
+          size="xl"
+          round
         />
       </div>
       <div class="col text-center text-h2">
@@ -25,57 +26,55 @@
       </div>
       <div class="col text-center">
         <q-btn 
-        @click="increaseCounter"
-         v-touch-repeat:300:300:300:300:50.
-         mouse="increaseCounter"
-        icon="add" 
-        size="xl"
-        round
+          @click="increaseCounter"
+          v-touch-repeat:300:300:300:300:50.mouse="increaseCounter"
+          icon="add" 
+          size="xl"
+          round
         />
       </div>
     </div>
     <div class="row">
       <q-btn 
-      @click="resetCounter"
+        @click="resetCounter"
         icon="restart_alt" 
         size="xl"
         round
-        />
+      />
     </div>
   </q-page>
 </template>
 
 <style scoped>
 .q-page {
-max-width: 700px;
-margin: 0  auto;
+  max-width: 700px;
+  margin: 0 auto;
 }
-
 </style>
+
 <script setup>
-
-/*
-imports
-*/
-
 import { reactive } from 'vue'
-
-/*
-data
-*/
 
 const data = reactive({
   counter: 0,
   name: ''
-  })
+})
 
-  const increaseCounter = () => {
-    data.counter++  
-  } 
-  const decreaseCounter = () => {
-    if(data.counter > 0) data.counter-- 
-  } 
-  const resetCounter = () => {
-data.counter = 0
-  } 
+const increaseCounter = () => {
+  data.counter++  
+} 
+
+const decreaseCounter = () => {
+  if (data.counter > 0) data.counter-- 
+} 
+
+const resetCounter = () => {
+  data.counter = 0
+} 
+
+const handlePan = e => {
+  console.log(e.delta.y)
+  if (e.delta.y < 0) increaseCounter()
+  else decreaseCounter()
+}
 </script>
